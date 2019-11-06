371ef716aeaa0e07d7cd2a080d8168bea0289e3b
Iris Riddell <riddell.iris@gmail.com>
2019-11-06 15:33:13 +1300

    Added a pretty purple box

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 
 import Header from './Header'
-import Drawer from './Drawer'
 import Footer from './Footer'
 import NavSidebar from './NavSidebar'
+import PurpleBox from './PurpleBox'
+import Drawer from './Drawer'
+
 
 const App = () => (
   <div className='body'>
     <Header />
     <Footer />
     <NavSidebar />
+    <PurpleBox />
     <Drawer />
   </div>
 )
